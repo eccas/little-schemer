15cb99ea01682dc2adb58020d405ac964ba0486d
eccas <joakim.sjunnebo@gmail.com>
2019-03-20 21:29:48 +0000

    Chapter 1-2

--- atom.rkt
+++ atom.rkt
@@ -3,3 +3,25 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 (atom? (quote ()))
+
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+
+(lat? '(a b))
+(lat? '())
+(lat? '(a b (c d)))
+
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? a (car lat))
+                (member? a (cdr lat)))))))
+
+(member? 'a '(b c a))
+(member? 'a '(b c ab))
+                   
