a76c33035712cd0df84eee160dc1d9b71160b69b
eccas <joakim.sjunnebo@gmail.com>
2019-03-29 21:28:31 +0000

    Rest of ch 4

--- chapter4.rkt
+++ chapter4.rkt
@@ -60,6 +60,74 @@
       ((< x y) #f)
       (else #t))))
 
+(define ^
+  (lambda (x y)
+    (cond
+      ((zero? y) 1)
+      (else (multiply x (^ x (sub1 y)))))))
+
+(define length
+  (lambda (lat)
+    (cond
+      ((null? lat) 0)
+      (else (add1 (length (cdr lat)))))))
+
+(define pick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (car lat))
+      (else (pick (sub1 n) (cdr lat))))))
+
+(define rempick
+  (lambda (n lat)
+    (cond
+      ((zero? (sub1 n)) (cdr lat))
+      (else (cons
+             (car lat)
+             (rempick (sub1 n) (cdr lat)))))))
+
+(define no-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((number? (car lat)) (no-nums (cdr lat)))
+      (else (cons (car lat) (no-nums (cdr lat)))))))
+
+(define all-nums
+  (lambda (lat)
+    (cond
+      ((null? lat) '())
+      ((number? (car lat)) (cons (car lat) (all-nums (cdr lat))))
+      (else (all-nums (cdr lat))))))
+
+(define eqan?
+  (lambda (a1 a2)
+    (cond
+      ((and (number? a1) (number? a2)) (= a1 a2))
+      ((or (number? a1) (number? a2)) #f)
+      (else (eq? a1 a2)))))
+      
+      
+(define occur
+  (lambda (a lat)
+    (cond
+      ((null? lat) 0)
+      ((eqan? a (car lat)) (add1 (occur a (cdr lat))))
+      (else (occur a (cdr lat))))))
+
+(define one?
+  (lambda (n)
+      (= n 1)))
+
+(define rempick2
+  (lambda (n lat)
+    (cond
+      ((one? n) (cdr lat))
+      (else (cons
+             (car lat)
+             (rempick2 (sub1 n) (cdr lat)))))))
+
+
 (add1 1)
 (sub1 4)
 (zero? 0)
@@ -80,4 +148,25 @@
 (= 10 20)
 (= 15 7)
 (= 3 3)
-
+(^ 1 1)
+(^ 2 3)
+(^ 5 3)
+(length '(a b c d e f))
+(length '())
+(length '(a b))
+(pick 4 '(a b c d e f))
+(pick 1 '(a b c d e f))
+(rempick 4 '(a b c d e f))
+(rempick 1 '(a b c d e f))
+(no-nums '(a 1 b 2 c 3 d e))
+(all-nums '(a 1 b 2 c 3 d e))
+(eqan? 1 2)
+(eqan? 1 1)
+(eqan? 1 'a)
+(eqan? 'a 'b)
+(eqan? 'a 'a)
+(occur 1 '(a f 2 1 j 3 1))
+(one? 1)
+(one? 12)
+(rempick2 4 '(a b c d e f))
+(rempick2 1 '(a b c d e f))
