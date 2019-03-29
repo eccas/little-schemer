0db0e708466d95c660ae737dfe9e471b7ef959b4
eccas <joakim.sjunnebo@gmail.com>
2019-03-29 08:33:54 +0000

    More chapter 4

--- chapter4.rkt
+++ chapter4.rkt
@@ -29,6 +29,37 @@
       (else
        (+ x (multiply x (sub1 y)))))))
 
+(define tup+
+  (lambda (tup1 tup2)
+    (cond
+      ((null? tup1) tup2)
+      ((null? tup2) tup1)
+      (else
+       (cons (+ (car tup1) (car tup2))
+                  (tup+
+                   (cdr tup1) (cdr tup2)))))))
+
+(define >
+  (lambda (x y)
+    (cond
+      ((zero? x) #f)
+      ((zero? y) #t)
+      (else (> (sub1 x) (sub1 y))))))
+
+(define <
+  (lambda (x y)
+    (cond
+      ((zero? y) #f)
+      ((zero? x) #t)
+      (else (< (sub1 x) (sub1 y))))))
+
+(define =
+  (lambda (x y)
+    (cond
+      ((> x y) #f)
+      ((< x y) #f)
+      (else #t))))
+
 (add1 1)
 (sub1 4)
 (zero? 0)
@@ -37,3 +68,16 @@
 (- 46 12)
 (addtup '(1 2 3))
 (multiply 3 4)
+(tup+ '(1 2 3) '(4 5 6))
+(tup+ '(1 2) '(4 5 6))
+(tup+ '(1 2 3) '(5 6))
+(> 10 20)
+(> 15 7)
+(> 3 3)
+(< 10 20)
+(< 15 7)
+(< 3 3)
+(= 10 20)
+(= 15 7)
+(= 3 3)
+
