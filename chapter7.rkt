48b826bce61eb10f0f6243719ea9c495179877f8
eccas <joakim.sjunnebo@gmail.com>
2019-04-06 11:33:45 +0100

    Last of ch 7

--- chapter7.rkt
+++ chapter7.rkt
@@ -1,12 +1,29 @@
 #lang racket
 
 (require "atom.rkt"
-         "chapter5.rkt")
+         "chapter5.rkt"
+         (only-in "chapter3.rkt" firsts))
 
 (provide set?
          makeset
          subset?
-         eqset?)
+         eqset?
+         intersect?
+         intersect
+         union
+         difference
+         intersectall
+         a-pair?
+         first
+         second
+         third
+         build
+         fun?
+         revpair
+         revrel
+         fullfun?
+         seconds
+         one-to-one?)
 
 (module redefs racket
   (provide member?
@@ -68,7 +85,108 @@
   (lambda (set1 set2)
     (and (subset? set1 set2)
          (subset? set2 set1))))
-   
+
+(define intersect?
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) #f)
+      (else (or (member? (car set1) set2)
+                (intersect? (cdr set1) set2))))))
+
+
+(define intersect
+  (lambda (set1 set2)
+    (cond
+      ((null? set1) '())
+      ((member? (car set1) set2)
+       (cons (car set1)
+             (intersect (cdr set1) set2)))
+      (else (intersect (cdr set1) set2)))))
+
+(define union
+  (lambda (set1  set2)
+    (cond
+      ((null? set1) set2)
+      ((member? (car set1) set2)
+       (union (cdr set1) set2))
+      (else (cons (car set1)
+                  (union (cdr set1) set2))))))
+      
+(define difference
+  (lambda (set1  set2)
+    (cond
+      ((null? set1) '())
+      ((member? (car set1) set2)
+       (difference (cdr set1) set2))
+      (else (cons (car set1)
+                  (difference (cdr set1) set2))))))
+
+(define intersectall
+  (lambda (lset)
+    (cond
+      ((null? (cdr lset)) (car lset))
+      (else (intersect (car lset)
+                       (intersectall (cdr lset)))))))
+
+(define a-pair?
+  (lambda (l)
+    (cond
+      ((atom? l) #f)
+      ((null? l) #f)
+      ((null? (cdr l)) #f)
+      ((null? (cdr (cdr l))) #t)
+      (else #f))))
+
+(define first
+  (lambda (p)
+    (car p)))
+
+(define second
+  (lambda (p)
+    (car (cdr p))))
+
+(define third
+  (lambda (l)
+    (car (cdr (cdr l)))))
+
+(define build
+  (lambda (s1 s2)
+    (cons s1
+          (cons s2
+                '()))))
+
+(define fun?
+  (lambda (rel)
+    (set? (firsts rel))))
+
+(define revpair
+  (lambda (p)
+    (build (second p)
+           (first p))))
+
+(define revrel
+  (lambda (rel)
+    (cond
+      ((null? rel) '())
+      (else (cons
+             (revpair (car rel))
+             (revrel (cdr rel)))))))
+
+(define seconds
+  (lambda (l)
+    (cond
+      ((null? l) '())
+      (else (cons (second (car l))
+                  (seconds (cdr l)))))))
+
+(define fullfun?
+  (lambda (rel)
+    (set? (seconds rel))))
+
+(define one-to-one?
+  (lambda (fun)
+    (fun? (revrel fun))))
+
 (module+ test
   (set? '(a b c))
   (set? '(a b c a))
@@ -83,4 +201,31 @@
   (eqset? '(a c b) '(a b c))
   (eqset? '(a c 4) '(4 a c))
   (eqset? '(a c b) '(a b c d))
+  (intersect? '(a b c) '(1 2 b))
+  (intersect? '(a b c) '(1 2 d))
+  (intersect '(a b c) '(1 2 b a))
+  (intersect '(a b c) '(1 2 d))
+  (union '(a b c) '(1 2 b a))
+  (union '(a b c) '(1 2 d))
+  (difference '(a b c) '(1 2 b a))
+  (difference '(a b c) '(1 2 d))
+  (intersectall '((a b c)(1 2 b d a)(h b a)))
+  (intersectall '((a b c)(1 2 d a)))
+  (intersectall '((a b c)))
+  (a-pair? '(a b))
+  (a-pair? '(a b c))
+  (a-pair? '(a (b c)))
+  (a-pair? '(a))
+  (a-pair? 'a)
+  (first '(a b))
+  (second '(a b))
+  (build 'a 'b)
+  (third '(a b c))
+  (fun? '((a b)(c d)(e f)(g h)))
+  (fun? '((a b)(c d)(e f)(a h)))
+  (revrel '((a b)(c d)(e f)(g h)))
+  (fullfun? '((a b)(c d)(e f)(a h)))
+  (fullfun? '((a b)(c d)(e f)(a b)))
+  (one-to-one? '((a b)(c d)(e f)(a h)))
+  (one-to-one? '((a b)(c d)(e f)(a b)))
   )
